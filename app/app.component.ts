08e0f70a6b9dbc60c1f86da94c60942aafe04d3a
Jacek Siciarek <siciarek@gmail.com>
2016-06-03 14:57:14 +0000

    Initial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,24 @@
 import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-
-import { DashboardComponent }  from './dashboard.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService }         from './hero.service';
 
 @Component({
-  selector: 'my-app',
-
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a [routerLink]="['Dashboard']">Dashboard</a>
-      <a [routerLink]="['Heroes']">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['app/app.component.css'],
-  directives: [ROUTER_DIRECTIVES],
-  providers: [
-    ROUTER_PROVIDERS,
-    HeroService,
-  ]
+    selector: 'app',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css'],
 })
-@RouteConfig([
-  { path: '/dashboard',  name: 'Dashboard',  component: DashboardComponent, useAsDefault: true },
-  { path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent },
-  { path: '/heroes',     name: 'Heroes',     component: HeroesComponent }
-])
 export class AppComponent {
-  title = 'Tour of Heroes';
+    title = 'Lista elementów';
+    items = [
+        {
+            "id": 100,
+            "name": "Sokół Millenium"
+        },
+        {
+            "id": 324,
+            "name": "Sokół Maltański"
+        },
+        {
+            "id": 544,
+            "name": "Barbarella"
+        }
+    ];
 }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
