5b47d305d1aa834c2a9aa378efcfa04cebe24d65
Jacek Siciarek <siciarek@gmail.com>
2016-06-06 11:17:36 +0000

    Add list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TabularComponent } from './tabular.component';
-
+import { ListComponent } from './list.component';
 
 @Component({
     selector: 'app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
     directives: [
+        ListComponent,
         TabularComponent
     ]
 })
---
+++ app/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'list',
+    templateUrl: 'app/list.component.html',
+    styleUrls: ['app/list.component.css'],
+})
+export class ListComponent {
+    items: Object[];
+}
